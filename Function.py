c29a3c5715a11f60f98cb23d18aac3dc9f20095c
Tseng, Po-Hsun <zengbs@gmail.com>
2019-12-30 10:25:33 +0800

    update

--- Function.py
+++ Function.py
@@ -1,9 +1,10 @@
+import scipy.integrate as integrate
 import numpy as np
 import math
+import  sys
 
 
-
-def GetSpringEnergy(N1, N2, N3, L1, L2, L3, x_external, y_external, z_external, Q, q, k, ep0):
+def GetSpringEnergy(N1, N2, N3, L1, L2, L3, x_external, y_external, z_external, Q, q, k, ep0 ):
     
     delta_x = L1/(N1-1)      # atoms spacing in x direction
     delta_y = L2/(N2-1)      # atoms spacing in y direction
@@ -14,18 +15,17 @@ def GetSpringEnergy(N1, N2, N3, L1, L2, L3, x_external, y_external, z_external, 
     x3 = 0.0                 # z coordinates of atoms
     Engy  = 0.0              # electro-magnetic energy
 
+    dV = delta_x*delta_y*delta_z
     
-    for i in range(1,N1+1):
-        for j in range(1,N2+1):
-            for k in range(1,N3+1):
+    for ii in range(0,N1):
+        for ij in range(0,N2):
+            for ik in range(0,N3):
 
-                x1 = (-L1/2) + (i-1)*delta_x
-                x2 = (-L2/2) + (j-1)*delta_y
-                x3 = (-L3/2) + (k-1)*delta_z
+                x1 = (-L1*0.5) + ii*delta_x
+                x2 = (-L2*0.5) + ij*delta_y
+                x3 = (-L3*0.5) + ik*delta_z
 
-                Engy += (1/(2*k)) * (q**2) * (0.25 * (1/math.pi) * (1/ep0) )**2 /   ( (x1 - x_external)**2 \
-                                                                                   +  (x2 - y_external)**2 \
-                                                                                   +  (x3 - z_external)**2  )
+                Engy += (0.5/k) * ( q**2 ) / ( (4 * math.pi * ep0 )**2 * ( (x1 - x_external)**2 +  (x2 - y_external)**2 +  (x3 - z_external)**2  ) )
 
     return Engy
                         
@@ -43,23 +43,19 @@ def GetDieletricEnergy(N1, N2, N3, L1, L2, L3, x_external, y_external, z_externa
 
     dV = delta_x*delta_y*delta_z
 
-    for i in range(1,N1+1):
-        for j in range(1,N2+1):
-            for k in range(1,N3+1):
+    integral = lambda x3, x2, x1: (0.5/k) * ( q**2/dV ) / ( (4 * math.pi * ep0 )**2 * ( (x1 - x_external)**2 \
+                                                                                     +  (x2 - y_external)**2 \
+                                                                                     +  (x3 - z_external)**2  ) )
+					                
+	
+    Engy = integrate.tplquad( integral, -L1/2, L1/2, lambda x1:-L2/2, lambda x1:L2/2, lambda x1, x2: -L3/2, lambda x1, x2: L3/2 )
 
-                x1 = (-L1/2) + (i-1)*delta_x
-                x2 = (-L2/2) + (j-1)*delta_y
-                x3 = (-L3/2) + (k-1)*delta_z
 
-                Engy += (1/(2*k)) * (q**2) / dV * (0.25 * (1/math.pi) * (1/ep0) )**2 /   ( (x1 - x_external)**2 \
-                                                                                        +  (x2 - y_external)**2 \
-                                                                                        +  (x3 - z_external)**2  ) * dV
-
-    return Engy
+    return Engy[0]
     
 
 
-# calculate vacuum energy
+# calculate energy within the vacuum cubic enclosed by boundary of dielectric
 
 def GetVacuumEnergy(N1, N2, N3, L1, L2, L3, x_external, y_external, z_external, Q, q, k, ep0):
 
@@ -73,16 +69,17 @@ def GetVacuumEnergy(N1, N2, N3, L1, L2, L3, x_external, y_external, z_external, 
     Engy  = 0.0              # electro-magnetic energy
 
     
-    for i in range(1,N1+1):
-        for j in range(1,N2+1):
-            for k in range(1,N3+1):
+    for ii in range(0,N1):
+        for ij in range(0,N2):
+            for ik in range(0,N3):
 
-                x1 = (-L1/2) + (i-1)*delta_x
-                x2 = (-L2/2) + (j-1)*delta_y
-                x3 = (-L3/2) + (k-1)*delta_z
+                x1 = (-L1*0.5) + ii*delta_x
+                x2 = (-L2*0.5) + ij*delta_y
+                x3 = (-L3*0.5) + ik*delta_z
 
                 Engy += 0.5 * (0.25 * (1/math.pi) * (1/ep0) )**2 /   ( (x1 - x_external)**2 \
                                                                     +  (x2 - y_external)**2 \
                                                                     +  (x3 - z_external)**2  )
 
     return Engy
+
--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 
 import numpy as np
 import math 
-import myFunction
+import Function
 
 ##################################################################
 # Assuming coordinate origin is at center of dielectric material #
@@ -12,6 +12,7 @@ import myFunction
 def main():
 
     #user parameters
+    ep0 =       1   # vacuum permmitivity
     L1 =        2   # size of dielectric material in x-direction
     L2 =        2   # size of dielectric material in y-direction
     L3 =        2   # size of dielectric material in z-direction
@@ -24,22 +25,35 @@ def main():
     Q =         1   # external free charge
     q =         1   # dipole charge per atom in dielectric material
     k =         1   # spring constant
-    ep0 =       1   # vacuum permmitivity
 
-    # calculate total electromagnetic energy
-    SpringEnergy    = myFunction.GetSpringEnergy   (N1, N2, N3, L1, L2, L3, x_sour, y_sour, z_sour, Q, q, k, ep0) 
-    DieletricEnergy = myFunction.GetDieletricEnergy(N1, N2, N3, L1, L2, L3, x_sour, y_sour, z_sour, Q, q, k, ep0) 
-    VacuumEnergy    = myFunction.GetVacuumEnergy   (N1, N2, N3, L1, L2, L3, x_sour, y_sour, z_sour, Q, q, k, ep0) 
+    f = open("data__compare","w+")
+    f.write("#method1: vacuum energy + spring energy\n")
+    f.write("#method2: vacuum energy + dieletric energy\n")
+    f.write("#%19s%20s%20s\n" %  ("number of atoms[0] method1 [1]", "method2 [2]", "relative error[3]") )
 
-    Engy_Method1 = VacuumEnergy + SpringEnergy
-    Engy_Method2 = VacuumEnergy + DieletricEnergy
+    for N in range(2,100):
 
-    print(Engy_Method1)
-    print(Engy_Method2)
+			 # assuming dielectric is a cubic
+             N1 = N
+             N2 = N
+             N3 = N
 
-    # print energy
-    print()
+             # calculate energy stored in springs
+             SpringEnergy    = Function.GetSpringEnergy   (N1, N2, N3, L1, L2, L3, x_sour, y_sour, z_sour, Q, q, k, ep0) 
+         
+         	 # calculate energy stored in polarized atoms
+             DieletricEnergy = Function.GetDieletricEnergy(N1, N2, N3, L1, L2, L3, x_sour, y_sour, z_sour, Q, q, k, ep0) 
+         
+         	 # calculate energy within the vacuum cubic enclosed by boundary of dielectric 
+             VacuumEnergy    = Function.GetVacuumEnergy   (N1, N2, N3, L1, L2, L3, x_sour, y_sour, z_sour, Q, q, k, ep0) 
+         
+             Engy_Method1 = VacuumEnergy + SpringEnergy
+         
+             Engy_Method2 = VacuumEnergy + DieletricEnergy
     
+             # dump data to disk
+             f.write( "%20.7d%20.7e%20.7e%20.7e\n" % (  N**3, Engy_Method1, Engy_Method2, 1-Engy_Method2/Engy_Method1 ) )
+
 
 # check the program is main function or not.
 if __name__ == "__main__":
